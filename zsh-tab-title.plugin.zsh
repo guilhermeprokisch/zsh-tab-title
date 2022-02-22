e5f3ed428f37a164a9d941d81e91a1705117bd15
Guilherme <guilherme.prokisch@gmail.com>
2022-02-22 12:28:58 -0300

    fix title

--- zsh-tab-title.plugin.zsh
+++ zsh-tab-title.plugin.zsh
@@ -31,10 +31,6 @@ function title {
       kitty @set-tab-title '~/'
     fi      
 
-    if [[ "$1"  ==  "exit" ]]; then
-      kitty @set-tab-title ''
-    fi      
-
   else
     case "$TERM" in
       cygwin|xterm*|putty*|rxvt*|ansi|${~ZSH_TAB_TITLE_ADDITIONAL_TERMS})
